42dba68ecf4d17ace7b68b191905f846796ea102
郭晓鹏 <guoxiaopeng@eascs.com>
2023-03-13 15:45:41 +0800

    feat(futures): WsMiniMarketTickerServeV2

--- v2/futures/websocket_service_v2.go
+++ v2/futures/websocket_service_v2.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/adshao/go-binance/v2/common"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -13,6 +14,28 @@ func WsUserDataServeV2(listenKey string, handler WsUserDataHandler) *common.WsCo
 	endpoint := fmt.Sprintf("%s/%s", getWsEndpoint(), listenKey)
 	wsHandler := func(message []byte) error {
 		event := new(WsUserDataEvent)
+		if err := json.Unmarshal(message, event); err != nil {
+			return err
+		}
+		handler(event)
+		return nil
+	}
+	return common.NewWsBuilder().
+		ProxyUrl(os.Getenv("HTTPS_PROXY")).
+		ProtoHandleFunc(wsHandler).
+		Heartbeat(func() []byte {
+			return []byte("1")
+		}, time.Second*30).
+		AutoReconnect().
+		WsUrl(endpoint).
+		Build()
+}
+
+// WsMiniMarketTickerServeV2 serve websocket that pushes 24hr rolling window mini-ticker statistics for a single symbol.
+func WsMiniMarketTickerServeV2(symbol string, handler WsMiniMarketTickerHandler) *common.WsConn {
+	endpoint := fmt.Sprintf("%s/%s@miniTicker", getWsEndpoint(), strings.ToLower(symbol))
+	wsHandler := func(message []byte) error {
+		event := new(WsMiniMarketTickerEvent)
 		if err := json.Unmarshal(message, event); err != nil {
 			return err
 		}
